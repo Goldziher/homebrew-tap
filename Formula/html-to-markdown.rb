0cf4b42e1fbc294eb5c3cda52c49af4a3d5adc9b
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-12-08 20:01:29 +0100

    chore(homebrew): update html-to-markdown to 2.11.4
    Auto-update from release v2.11.4

--- Formula/html-to-markdown.rb
+++ Formula/html-to-markdown.rb
@@ -1,8 +1,8 @@
 class HtmlToMarkdown < Formula
   desc "High-performance HTML to Markdown converter powered by Rust"
   homepage "https://github.com/Goldziher/html-to-markdown"
-  url "https://github.com/Goldziher/html-to-markdown/archive/v2.11.3.tar.gz"
-  sha256 "f80b708adf5ffe4510d35443e9026f7f9045f05657a280e42fcfdbbedb65d190"
+  url "https://github.com/Goldziher/html-to-markdown/archive/v2.11.4.tar.gz"
+  sha256 "5417914e0e58128822f1668d9fd24e5ce968c6acf656261e78a72ed4660001d7"
   license "MIT"
 
   depends_on "rust" => :build
