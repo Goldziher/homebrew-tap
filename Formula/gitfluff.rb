b758eb2f4e726390a006c2923485eee139087bde
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-01-18 08:34:04 +0100

    gitfluff 0.8.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/gitfluff.rb
+++ Formula/gitfluff.rb
@@ -1,8 +1,8 @@
 class Gitfluff < Formula
   desc "Commit message linter with presets and cleanup automation"
   homepage "https://github.com/Goldziher/gitfluff"
-  url "https://github.com/Goldziher/gitfluff/archive/v0.7.1.tar.gz"
-  sha256 "0418ac15d3a9fecbfec5a3d6afed9d6d044467fa8da0b10c9f2381a767857f4c"
+  url "https://github.com/Goldziher/gitfluff/archive/v0.8.0.tar.gz"
+  sha256 "b3fcb2fdbaa8d08203d002a1edbc0819b6f81a4783bfe4f2d28db88e5c66116f"
   license "MIT"
 
   depends_on "rust" => :build
