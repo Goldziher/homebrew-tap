222a9b9d5a4c2761035f89107dcd81b02a93c354
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-04-07 21:02:08 +0200

    scythe 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/scythe.rb
+++ Formula/scythe.rb
@@ -4,8 +4,8 @@
 class Scythe < Formula
   desc "SQL compiler and linter - generate type-safe code from SQL"
   homepage "https://github.com/Goldziher/scythe"
-  url "https://github.com/Goldziher/scythe/archive/v0.2.0.tar.gz"
-  sha256 "80246e84b62c3324060fd6d20ad6c3caf86615c47b0d732b96277adc0ff0a775"
+  url "https://github.com/Goldziher/scythe/archive/v0.3.0.tar.gz"
+  sha256 "b8707bb0c05135f7372e881c3aaa3f1d5a7fb8102ba3c043d6e82c0557a01b85"
   license "MIT"
   head "https://github.com/Goldziher/scythe.git", branch: "main"
 
